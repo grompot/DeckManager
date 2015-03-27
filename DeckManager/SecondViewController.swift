77d92e99078cf66ecd03847f4147d189b694aa71
George Rompot <grompot@ameitech.net>
2015-03-26 20:46:16 -0500

    Initial Commit

---
+++ DeckManager/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DeckManager
+//
+//  Created by George Rompot on 3/26/15.
+//  Copyright (c) 2015 George Rompot. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
