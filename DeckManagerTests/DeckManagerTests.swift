acef7f19484e784da306fcb525ad107a07ceb8e0
George Rompot <grompot@ameitech.net>
2015-04-10 09:09:29 -0500

    Test remote omit

--- DeckManagerTests/DeckManagerTests.swift
+++ DeckManagerTests/DeckManagerTests.swift
@@ -12,10 +12,12 @@ import XCTest
 
 
 class DeckManagerTests: XCTestCase {
-    
+
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
     
     override func tearDown() {
